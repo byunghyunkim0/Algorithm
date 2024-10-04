8c833ece4524820c874e42ba89a132f4983f1df5
byunghyunkim0 <139294001+byunghyunkim0@users.noreply.github.com>
2024-10-04 22:37:32 +0900

    [Gold IV] Title: 사이클 게임, Time: 536 ms, Memory: 151284 KB -BaekjoonHub

--- 백준/Gold/20040. 사이클 게임/사이클 게임.java
+++ 백준/Gold/20040. 사이클 게임/사이클 게임.java
@@ -1,8 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.StringTokenizer;
 
 // 사이클 게임
@@ -19,7 +17,6 @@ public class Main {
         for (int i = 1; i <= n; i++) {
             parent[i] = i;
         }
-        List<Integer> result = new ArrayList<>();
         for (int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
@@ -27,14 +24,11 @@ public class Main {
             if (find(a) != find(b)) {
                 union(a, b);
             } else {
-                result.add(i + 1);
+                System.out.println(i + 1);
+                return;
             }
         }
-        if (result.isEmpty()) {
-            System.out.println(0);
-        } else {
-            System.out.println(result.get(0));
-        }
+        System.out.println(0);
     }
 
     static int find(int x) {
