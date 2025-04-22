05cf8165c9a69596e0d52c1240b211067f622120
byunghyunkim0 <139294001+byunghyunkim0@users.noreply.github.com>
2025-04-22 19:37:09 +0900

    [Gold II] Title: 물통, Time: 1784 ms, Memory: 309408 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/14867. 물통/물통.java
+++ 백준/Gold/14867. 물통/물통.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
@@ -15,19 +16,21 @@ public class Main {
             this.b = b;
         }
     }
+    static int a;
+    static int b;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int a = Integer.parseInt(st.nextToken());
-        int b = Integer.parseInt(st.nextToken());
+        a = Integer.parseInt(st.nextToken());
+        b = Integer.parseInt(st.nextToken());
         int c = Integer.parseInt(st.nextToken());
         int d = Integer.parseInt(st.nextToken());
 
-        boolean[][] visited = new boolean[a + 1][b + 1];
-        visited[0][0] = true;
-
         Queue<Work> works = new LinkedList<>();
         works.add(new Work(0, 0));
+
+        HashMap<String, Integer> map = new HashMap<>();
+        map.put("0_0", 0);
 
         int time = 0;
         while (!works.isEmpty()) {
@@ -39,37 +42,44 @@ public class Main {
                     return;
                 }
 
-                if (!visited[a][cur.b]) {
-                    visited[a][cur.b] = true;
+                String[] water = getWater(cur.a, cur.b);
+
+                if (!map.containsKey(water[0])) {
+                    map.put(water[0], 0);
                     works.add(new Work(a, cur.b));
                 }
-                if (!visited[cur.a][b]) {
-                    visited[cur.a][b] = true;
+                if (!map.containsKey(water[1])) {
+                    map.put(water[1], 0);
                     works.add(new Work(cur.a, b));
                 }
 
-                if (!visited[0][cur.b]) {
-                    visited[0][cur.b] = true;
+                if (!map.containsKey(water[2])) {
+                    map.put(water[2], 0);
                     works.add(new Work(0, cur.b));
                 }
-                if (!visited[cur.a][0]) {
-                    visited[cur.a][0] = true;
+                if (!map.containsKey(water[3])) {
+                    map.put(water[3], 0);
                     works.add(new Work(cur.a, 0));
                 }
 
                 int sum = cur.a + cur.b;
-                if (!visited[Math.min(sum, a)][Math.max(sum - a, 0)]) {
-                    visited[Math.min(sum, a)][Math.max(sum - a, 0)] = true;
+                if (!map.containsKey(water[4])) {
+                    map.put(water[4], 0);
                     works.add(new Work(Math.min(sum, a), Math.max(sum - a, 0)));
                 }
 
-                if (!visited[Math.max(sum - b, 0)][Math.min(sum, b)]) {
-                    visited[Math.max(sum - b, 0)][Math.min(sum, b)] = true;
+                if (!map.containsKey(water[5])) {
+                    map.put(water[5], 0);
                     works.add(new Work(Math.max(sum - b, 0), Math.min(sum, b)));
                 }
             }
             time++;
         }
         System.out.println(-1);
+    }
+
+    static String[] getWater(int x, int y) {
+        int sum = x + y;
+        return new String[] {"a_" + y, x + "_b", "0_" + y, x + "_0", Math.min(sum, a) + "_" + Math.max(sum - a, 0), Math.max(sum - b, 0) + "_" + Math.min(sum, b)};
     }
 }
